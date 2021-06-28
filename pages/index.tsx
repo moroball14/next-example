8872d262ca0fbe3b736dc7e28fe3a58cb7b82093
moroball14 <daiki.mr.0330@gmail.com>
2021-06-29 07:45:29 +0900

    styled index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import axios from 'axios';
 import {GetStaticProps} from 'next';
 import Head from 'next/head';
+import styled from 'styled-components';
 import {EntryList} from '../components/EntryList';
+import {SideNavigation} from '../components/SideNavigation';
 import {BlogType} from '../types/BlogType';
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 312px 1fr;
+  height: 100%;
+`;
+
+const Main = styled.main`
+  background: #f8f9fc;
+  padding-top: 127px;
+  padding-left: 80px;
+`;
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await axios.get(
@@ -26,7 +40,12 @@ const Index: React.FC<{blogs: BlogType[]}> = ({blogs}) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <EntryList blogData={blogs} />
+      <Wrapper>
+        <SideNavigation />
+        <Main>
+          <EntryList blogData={blogs} />
+        </Main>
+      </Wrapper>
     </div>
   );
 };
