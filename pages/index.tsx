7b35d58557d4027bac854f8b9ae1cfe390aba183
moroball14 <daiki.mr.0330@gmail.com>
2021-06-28 22:53:29 +0900

    fix eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import {FunctionComponent} from 'react';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home: FunctionComponent = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -66,4 +67,6 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
