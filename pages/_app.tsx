36a87544f3b31ea198ce9c64464ef20e9ec8b868
moroball14 <daiki.mr.0330@gmail.com>
2021-06-28 23:31:55 +0900

    reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type {AppProps} from 'next/app';
 import '../styles/globals.css';
+import '../styles/reset.css';
 
 const MyApp = ({Component, pageProps}: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
