5581f23d003d9911a82f3eb6c7559608bbf6914a
moroball14 <daiki.mr.0330@gmail.com>
2021-07-04 22:26:54 +0900

    ヘッダーのモバイル対応

--- components/base/Header.tsx
+++ components/base/Header.tsx
@@ -5,24 +5,12 @@ import {HeaderItem} from './HeaderItem';
 
 export const Header: React.FC = () => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-white">
+    <nav className="flex flex-col items-center justify-center flex-wrap bg-white lg:justify-between lg:flex-row">
       <div className="flex items-center flex-no-shrink">
         <Image src={logo} alt={'fwywd'} width={200} height={100} />
       </div>
-      {/* <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
-          <svg
-            className="h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div> */}
-      <div className="block flex lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
+      <div className="block flex items-center lg:w-auto">
+        <div className="flex flex-row justify-between px-12 w-screen lg:w-auto lg:px-0">
           <HeaderItem title={'ABOUT'} />
           <HeaderItem title={'SKILLS'} />
           <HeaderItem title={'VALUES'} />
--- components/base/HeaderItem.tsx
+++ components/base/HeaderItem.tsx
@@ -8,7 +8,7 @@ export const HeaderItem: React.FC<Props> = ({title}) => {
   return (
     <a
       href="#responsive-header"
-      className="block text-2xl mt-4 lg:inline-block lg:mt-0 text-teal-light hover:text-teal-dark mr-4"
+      className="block text-1xl inline-block text-teal-light hover:text-teal-dark lg:mr-4 lg:text-2xl"
     >
       {title}
     </a>
