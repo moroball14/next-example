1bcf1294f3846d11e9d17be85adcf94ee56e403b
moroball14 <daiki.mr.0330@gmail.com>
2021-07-11 22:35:42 +0900

    pc対応

--- components/base/Skills.tsx
+++ components/base/Skills.tsx
@@ -6,15 +6,19 @@ import {SkillBarBlock} from './SkillBarBlock';
 
 export const Skills: React.FC = () => {
   return (
-    <div className="px-4 py-2 pb-16 bg-skillsSp bg-cover space-y-4 bg-no-repeat md:px-10">
-      <Title id={'skills'} titleJa={'スキル'} titleEn={'Skills'} />
-      <div className="flex flex-wrap content-center">
-        <Image src={skill} alt={'skills'} />
+    <div className="px-4 py-4 pb-16 bg-skillsSp bg-cover bg-no-repeat md:px-10 md:py-14">
+      <Title id={'skills'} titleJa={'スキル'} titleEn={'SKILLS'} />
+      <div className="flex flex-col space-y-4 md:flex-row md:space-x-8">
+        <div className="flex flex-wrap content-center">
+          <Image src={skill} alt={'skills'} />
+        </div>
+        <div className="flex flex-col space-y-4">
+          <div className="break-words leading-loose text-gray-500">
+            スキルはあるのかないのかわかりませんが、一応なんとかフロントエンドを一年とバックエンドを一年やってます
+          </div>
+          <SkillBarBlock />
+        </div>
       </div>
-      <div className="break-words leading-loose text-gray-500">
-        スキルはあるのかないのかわかりませんが、一応なんとかフロントエンドを一年とバックエンドを一年やってます
-      </div>
-      <SkillBarBlock />
     </div>
   );
 };
