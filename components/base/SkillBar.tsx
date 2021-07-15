0756554755b3cf3a681f79e9625e19eff4bbf98e
moroball14 <daiki.mr.0330@gmail.com>
2021-07-15 22:34:07 +0900

    tailwind-styled-components導入

--- components/base/SkillBar.tsx
+++ components/base/SkillBar.tsx
@@ -6,7 +6,7 @@ export const SkillBar: React.FC = () => {
       <div className="text-teal-dark text-2xl">React</div>
       <div className="flex justify-between w-full space-x-2">
         <div className="w-full bg-teal-light rounded">
-          <div className="w-1/2 h-full bg-teal rounded"></div>
+          <div className="w-1/5 h-full bg-teal rounded"></div>
         </div>
         <div className="text-teal">20%</div>
       </div>
--- components/base/Title.tsx
+++ components/base/Title.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import tw from 'tailwind-styled-components';
+
+const Container = tw.div`
+  flex
+  justify-center
+  py-6
+`;
 
 interface P {
   id: string;
@@ -8,11 +15,11 @@ interface P {
 
 export const Title: React.FC<P> = ({id, titleJa, titleEn}) => {
   return (
-    <div className="flex justify-center py-6" id={id}>
+    <Container id={id}>
       <div className="flex flex-col space-y-1 self-center md:flex-row md:space-x-2">
         <div className="font-bold text-3xl text-center">{titleJa}</div>
         <div className="text-teal-dark text-2xl text-center">{titleEn}</div>
       </div>
-    </div>
+    </Container>
   );
 };
